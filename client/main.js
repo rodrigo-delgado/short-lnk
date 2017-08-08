a9a61e735e089b3a0e108a6ca29a04f3b55d67d9
rodrigo-delgado <rod.delgado85@gmail.com>
2017-08-08 19:42:25 +1200

    add NotFound component to react router

--- client/main.js
+++ client/main.js
@@ -6,11 +6,15 @@ import { Router, Route, browserHistory } from 'react-router';
 
 import Signup from '../imports/ui/Signup';
 import Link from '../imports/ui/Link';
+import NotFound from '../imports/ui/NotFound';
+
 
 const routes = (
   <Router history={browserHistory}>
     <Route path="/signup" component={Signup} />
     <Route path="/links" component={Link} />
+    <Route path="*" component={NotFound} />
+
   </Router>
 );
 
