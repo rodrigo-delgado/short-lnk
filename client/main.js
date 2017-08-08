c939d514a23e7a976f7df12434a01eb8dc3c0d20
rodrigo-delgado <rod.delgado85@gmail.com>
2017-08-08 19:38:35 +1200

    add react-router

--- client/main.js
+++ client/main.js
@@ -10,6 +10,7 @@ import Link from '../imports/ui/Link';
 const routes = (
   <Router history={browserHistory}>
     <Route path="/signup" component={Signup} />
+    <Route path="/links" component={Link} />
   </Router>
 );
 
